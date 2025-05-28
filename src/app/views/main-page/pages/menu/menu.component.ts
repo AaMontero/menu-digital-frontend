98d754e75c6516ab2a14b9a4676ff8934a8d1573
AaMontero <heatdkwow@gmail.com>
2025-05-28 00:19:41 -0500

    Cambio 2805

---
+++ src/app/views/main-page/pages/menu/menu.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { MenuService } from '../../../../core/services/menu.service';
+import { Menu } from '../../../../core/models/menu.module';
+@Component({
+  selector: 'app-menu',
+  imports: [
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css',
+})
+export class MenuComponent {
+  private menuService = inject(MenuService);
+  menus: Menu[] = [];
+  ngOnInit(): void {
+    this.menuService.getMenu().subscribe((data) => {
+      this.menus = data;
+    });
+  }
+}
